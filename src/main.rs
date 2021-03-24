a076b2bf24bcff061f9bdebbbe1ef6ebdf2ef602
Moritz Eck <moritz.eck@gmail.com>
2021-03-24 22:15:42 +0100

    adds basic ci + container image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let statement = statement();
+    println!("{:?}", statement);
+}
+
+fn statement() -> &'static str {
+    "hello world from rust!"
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::statement;
+
+    #[test]
+    fn it_should_test_main() {
+        let statement = statement();
+        assert_eq!(statement, "hello world from rust!");
+    }
 }
